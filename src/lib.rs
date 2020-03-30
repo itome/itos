783760b09fbd487ced9d1c7a0540df77aeea9956
Takeshi Tsukamoto <itometeam@gmail.com>
2020-03-30 17:47:58 +0900

    Reformat code

--- src/lib.rs
+++ src/lib.rs
@@ -28,78 +28,78 @@ extern "C" fn eh_personality() {}
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-  print!("Aborting: ");
-  if let Some(p) = info.location() {
-    println!(
-      "line {}, file {}: {}",
-      p.line(),
-      p.file(),
-      info.message().unwrap()
-    );
-  } else {
-    println!("no information available.");
-  }
-  abort();
+    print!("Aborting: ");
+    if let Some(p) = info.location() {
+        println!(
+            "line {}, file {}: {}",
+            p.line(),
+            p.file(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("no information available.");
+    }
+    abort();
 }
 #[no_mangle]
 extern "C" fn abort() -> ! {
-  loop {
-    unsafe {
-      asm!("wfi"::::"volatile");
+    loop {
+        unsafe {
+            asm!("wfi"::::"volatile");
+        }
     }
-  }
 }
 
 #[no_mangle]
 extern "C" fn kmain() {
-  let mut my_uart = uart::Uart::new(0x1000_0000);
+    let mut my_uart = uart::Uart::new(0x1000_0000);
 
-  my_uart.init();
+    my_uart.init();
 
-  println!("This is my operating system!");
-  println!("I'm so awesome. If you start typing something, I'll show you what you typed!");
+    println!("This is my operating system!");
+    println!("I'm so awesome. If you start typing something, I'll show you what you typed!");
 
-  loop {
-    if let Some(c) = my_uart.get() {
-      match c {
-        8 => {
-          print!("{}{}{}", 8 as char, ' ', 8 as char);
-        }
-        10 | 13 => {
-          println!();
-        }
-        0x1b => {
-          if let Some(next_byte) = my_uart.get() {
-            if next_byte == 91 {
-              // This is a right bracket! We're on our way!
-              if let Some(b) = my_uart.get() {
-                match b as char {
-                  'A' => {
-                    println!("That's the up arrow!");
-                  }
-                  'B' => {
-                    println!("That's the down arrow!");
-                  }
-                  'C' => {
-                    println!("That's the right arrow!");
-                  }
-                  'D' => {
-                    println!("That's the left arrow!");
-                  }
-                  _ => {
-                    println!("That's something else.....");
-                  }
+    loop {
+        if let Some(c) = my_uart.get() {
+            match c {
+                8 => {
+                    print!("{}{}{}", 8 as char, ' ', 8 as char);
                 }
-              }
+                10 | 13 => {
+                    println!();
+                }
+                0x1b => {
+                    if let Some(next_byte) = my_uart.get() {
+                        if next_byte == 91 {
+                            // This is a right bracket! We're on our way!
+                            if let Some(b) = my_uart.get() {
+                                match b as char {
+                                    'A' => {
+                                        println!("That's the up arrow!");
+                                    }
+                                    'B' => {
+                                        println!("That's the down arrow!");
+                                    }
+                                    'C' => {
+                                        println!("That's the right arrow!");
+                                    }
+                                    'D' => {
+                                        println!("That's the left arrow!");
+                                    }
+                                    _ => {
+                                        println!("That's something else.....");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    print!("{}", c as char);
+                }
             }
-          }
         }
-        _ => {
-          print!("{}", c as char);
-        }
-      }
     }
-  }
 }
 
 pub mod uart;
